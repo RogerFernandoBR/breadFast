4a97c5573ed4f0f5b240dbac39ac8c561757be28
RogerFernandoBR <rogerfernandodeolveira@gmail.com>
2023-08-16 23:13:24 -0300

    Inicio da home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'breadFast';
+
+  constructor() {}
+
+    ngAfterViewInit() {      
+      let deg = 9.8;
+      for (let i = 1; i <= 49; i++) {
+        let div = document.querySelector(".stamp div:nth-child(" + i + ")") as HTMLElement;
+        div.style.transform = 'rotate(' + deg + 'deg)';
+        deg = deg + 9.8;
+      }
+    }
 }
