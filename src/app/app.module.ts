18350278e1adec92c4fa68a9ba58fb62ed4ad632
RogerFernandoBR <rogerfernandodeolveira@gmail.com>
2023-08-20 10:49:40 -0300

    Criado os componentes e as páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContacComponent } from './pages/contac/contac.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SocialsComponent } from './components/socials/socials.component';
+import { LogoComponent } from './componets/logo/logo.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContacComponent,
+    ProductsComponent,
+    AboutComponent,
+    SocialsComponent,
+    LogoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/contac/contac.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contac',
+  templateUrl: './contac.component.html',
+  styleUrls: ['./contac.component.scss']
+})
+export class ContacComponent {
+
+}
